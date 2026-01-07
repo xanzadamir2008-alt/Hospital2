6d31654d56478705e77c2294d915ca0a5edc3b30
xanzadamir2008-alt <xanzadamir2008@gmail.com>
2026-01-07 17:51:03 +0500

    Update Doctor.java

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,52 +1,29 @@
-public class Doctor {
-    private int id;
-    private String name;
-    private String specialization;
-    private int experienceYears;
+public class Patient extends Person {
+    private int age;
+    private String diagnosis;
 
-    public Doctor() {
-        this.id = 0;
-        this.name = "Unknown";
-        this.specialization = "General";
-        this.experienceYears = 0;
+    public Patient(int id, String name, int age, String diagnosis) {
+        super(id, name);
+        setAge(age);
+        this.diagnosis = diagnosis;
     }
 
-    public Doctor(int id, String name, String specialization, int experienceYears) {
-        this.id = id;
-        this.name = name;
-        this.specialization = specialization;
-        this.experienceYears = experienceYears;
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        }
     }
 
-    // Getters & Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getSpecialization() { return specialization; }
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
+    @Override
+    public void work() {
+        System.out.println("Patient " + name + " is receiving treatment");
     }
 
-    public int getExperienceYears() { return experienceYears; }
-    public void setExperienceYears(int experienceYears) {
-        this.experienceYears = experienceYears;
-    }
-
-    // Additional methods
-    public boolean isSeniorDoctor() {
-        return experienceYears >= 10;
-    }
-
-    public void addExperience(int years) {
-        this.experienceYears += years;
-    }
-
+    @Override
     public String toString() {
-        return "Doctor [id=" + id + ", name=" + name +
-                ", specialization=" + specialization +
-                ", experienceYears=" + experienceYears + "]";
+        return "Patient [name=" + name +
+                ", age=" + age +
+                ", diagnosis=" + diagnosis + "]";
     }
 }
+
