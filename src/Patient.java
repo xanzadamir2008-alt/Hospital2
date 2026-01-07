5c5c9d474c8352627b8d04d755c877dd97939ed2
xanzadamir2008-alt <xanzadamir2008@gmail.com>
2026-01-07 17:48:28 +0500

    Refactor Patient class to extend Person
    Refactored Patient class to extend Person class and removed unused fields and methods.

--- src/Patient.java
+++ src/Patient.java
@@ -1,54 +1,29 @@
-public class Patient {
-    private int id;
-    private String name;
+public class Patient extends Person {
     private int age;
     private String diagnosis;
 
-    // Default constructor
-    public Patient() {
-        this.id = 0;
-        this.name = "Unknown";
-        this.age = 0;
-        this.diagnosis = "Not diagnosed";
-    }
-
-    // Parameterized constructor
     public Patient(int id, String name, int age, String diagnosis) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+        super(id, name);
+        setAge(age);
         this.diagnosis = diagnosis;
     }
 
-    // Getters & Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public int getAge() { return age; }
     public void setAge(int age) {
         if (age >= 0) {
             this.age = age;
         }
     }
 
-    public String getDiagnosis() { return diagnosis; }
-    public void setDiagnosis(String diagnosis) { this.diagnosis = diagnosis; }
-
-    // Additional methods
-    public boolean isAdult() {
-        return age >= 18;
+    @Override
+    public void work() {
+        System.out.println("Patient " + name + " is receiving treatment");
     }
 
-    public void updateDiagnosis(String newDiagnosis) {
-        this.diagnosis = newDiagnosis;
-    }
-
+    @Override
     public String toString() {
-        return "Patient [id=" + id + ", name=" + name +
-                ", age=" + age + ", diagnosis=" + diagnosis + "]";
+        return "Patient [name=" + name +
+                ", age=" + age +
+                ", diagnosis=" + diagnosis + "]";
     }
 }
 
